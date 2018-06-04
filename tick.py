7b99e1492718a238146343c360e99d1bc3d8e488
Thielith <aydensgame@gmail.com>
2018-06-04 14:02:56 -0700

    mmmmmmmmmmmmmm

--- tick.py
+++ tick.py
@@ -7,30 +7,10 @@ db = MySQLdb.connect(host="localhost",  # your host
 						 db="test_dwarven")  # name of the database
 cur = db.cursor()
 
-if sys.argv[1] != None:
-		if sys.argv[1] == "updatePlayerPos":
-			sqlCommand = ""
-			print(sys.argv[2])
-			#connection.execute(sqlCommand)
-			#print("commit update")
-			#db.commit();
-			#print("close")
-			#db.close();
-	
-		elif sys.argv[1] == "updateNewPlayerPos":
-			print("updating new X and Y of player with ID: " + sys.argv[2])
-			collumm = ["NewXPos", "NewYPos"]
-			values = [sys.argv[3], sys.argv[4]]
-			updateDatabaseData(cur, "playerPos", collumm, values, sys.argv[2])
-			print("commit update")
-			db.commit();
-			print("close")
-			db.close();
-	
-		elif sys.argv[1] == "updateStatus":
-			print("adding statuses")
-			updateStatusDB(cur, sys.argv[2], sys.argv[3], sys.argv[4])
-			print("commit update")
-			db.commit();
-			print("close")
-			db.close();
+#playerX,   playerY,   newX,      newY,      typeX,      typeY,     diffX, diffY, lines, diagonals, playerID
+sqlCommand = "SELECT * FROM playerPos;"
+print(cur.execute(sqlCommand))
+
+print("close")
+db.close();
+
